ca89b990c8fcc96e6172493f92c3715cd3ebec44
Senorsen <senorsen.zhang@gmail.com>
2016-07-21 11:39:20 +0800

    return full path

--- src/providers/QQMusic.ts
+++ src/providers/QQMusic.ts
@@ -146,7 +146,7 @@ export default class QQMusicProvider extends BaseProvider {
     }
 
     private getArtworkUrl(imgId: string): string {
-        return `//i.gtimg.cn/music/photo/mid_album_300/${imgId[imgId.length - 2]}/${imgId[imgId.length - 1]}/${imgId}.jpg`
+        return `https://i.gtimg.cn/music/photo/mid_album_300/${imgId[imgId.length - 2]}/${imgId[imgId.length - 1]}/${imgId}.jpg`
     }
 
     public async getPlayingUrl(songId: string, overseas: boolean): Promise<string> {
