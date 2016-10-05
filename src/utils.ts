c74f6100406b058e6423b2bc6b08e1417da026d2
Senorsen <senorsen.zhang@gmail.com>
2016-10-05 14:24:28 +0800

    use new regexp

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,7 @@ function guessFromSongListNetease(url: Url.Url): string {
 
     const id = qs.parse(url.query).id
     const pathname = url.pathname
-    const parseIdFromPath = (s: string) => s.split('/')[2];
+    const parseIdFromPath = (s: string) => /playlist\/(\d+)\/?/.exec(s)[1];
     if (_.some(matches, it => it.test(pathname))) {
         return id || parseIdFromPath(pathname);
     }
