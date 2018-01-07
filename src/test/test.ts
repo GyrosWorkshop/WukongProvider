3ab2ae879e9e8d72435fc891d97e682b52a4133d
Richard <richard9372@gmail.com>
2018-01-07 11:00:47 +0800

    Initialize redis in provider for test, flush redis before run test.

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,14 @@
+const serverConfig = require('../../server-config.json')
+import * as Redis from 'redis'
+// Flush db before run test.
+Redis.createClient(serverConfig.redis).flushdb()
+
 if (process.env.api) {
     require('./ApiTest')
 } else {
-    // require('./QQMusicTest')
-    require('./NeteaseCloudMusicTest')
+
+    require('./QQMusicTest')
+    // require('./NeteaseCloudMusicTest')
     // require('./XiamiTest')
     require('./utilsTest')
 }
