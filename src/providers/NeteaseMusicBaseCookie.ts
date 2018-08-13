16a05b70690beac69c0decf3bd8f6a898ac2b01b
Richard <richard9372@gmail.com>
2018-08-13 23:22:26 +0800

    make ci pass

--- src/providers/NeteaseMusicBaseCookie.ts
+++ src/providers/NeteaseMusicBaseCookie.ts
@@ -3,7 +3,7 @@
 function randomString(pattern: string, length: number) {
     return Array.apply(null, {length: length}).map(() => (pattern[Math.floor(Math.random() * pattern.length)])).join('')
 }
-const jsessionid = randomString('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ\\/+',176) + ':' + (new Date).getTime()
-const nuid = randomString('0123456789abcdefghijklmnopqrstuvwxyz',32)
+const jsessionid = randomString('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ\\/+', 176) + ':' + (new Date).getTime()
+const nuid = randomString('0123456789abcdefghijklmnopqrstuvwxyz', 32)
 const baseCookie: string = `JSESSIONID-WYYY=${jsessionid}; _iuqxldmzr_=32; _ntes_nnid=${nuid},${(new Date).getTime()}; _ntes_nuid=${nuid}`
 export default baseCookie
